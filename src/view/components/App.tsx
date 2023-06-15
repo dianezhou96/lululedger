c57fa5774c11c85fdc936ed0ff48a5cb361d7d36
Alex Huang <huangalex@hotmail.com>
2023-06-15 01:06:28 -0700

    Use hashrouter to allow reloading

--- src/view/components/App.tsx
+++ src/view/components/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Nav from './Nav';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Layout, theme } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 
