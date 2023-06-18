a9ba7332469dfc1b5e7ce3bcf8c7025018adba43
Alex Huang <huangalex@hotmail.com>
2023-06-18 11:44:40 -0700

    Fix loading screen sizing

--- src/view/components/Loading.tsx
+++ src/view/components/Loading.tsx
@@ -3,8 +3,24 @@ import React from "react";
 
 export const Loading: React.FC = () => {
   return (
-    <div style={{ marginTop: "50px", textAlign: "center" }}>
-      <Spin />
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyItems: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spin size="large" style={{ transform: "scale(2.5)" }} />
+      </div>
     </div>
   );
 };
