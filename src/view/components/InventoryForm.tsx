28c8e79b5a838b8416ad6177a8b0b1fe874dc2bd
Diane <dianezhou96@gmail.com>
2023-11-18 13:48:52 -0800

    Add form saved alert

--- src/view/components/InventoryForm.tsx
+++ src/view/components/InventoryForm.tsx
@@ -33,7 +33,7 @@ export const InventoryForm: React.FC = () => {
   const success = useCallback(() => {
     messageApi.open({
       type: "success",
-      content: "Added to cart!",
+      content: "Inventory saved!",
     });
   }, []);
 
@@ -45,204 +45,230 @@ export const InventoryForm: React.FC = () => {
         "Content-Type": "application/json",
       },
     });
-    setFormValues(values);
     success();
+    setLoading(true);
+    getFormValues();
   };
 
-  return loading ? (
-    <Loading />
-  ) : (
-    <Form
-      labelCol={{
-        span: 6,
-      }}
-      wrapperCol={{
-        span: 18,
-      }}
-      form={form}
-      name="inventory-form"
-      style={{
-        maxWidth: 600,
-      }}
-      autoComplete="off"
-      initialValues={formValues}
-      onFinish={onSubmit}
-    >
-      <Form.List name="items">
-        {(fields, { add, remove }) => (
-          <div
-            style={{
-              display: "flex",
-              rowGap: 16,
-              flexDirection: "column",
-            }}
-          >
-            {fields.map((field) => (
-              <Card
-                size="small"
-                title={`Item ${field.name + 1}`}
-                key={field.key}
-                extra={
-                  <CloseOutlined
-                    onClick={() => {
-                      remove(field.name);
-                    }}
-                  />
-                }
+  return (
+    <>
+      {contextHolder}
+      {loading ? (
+        <Loading />
+      ) : (
+        <Form
+          labelCol={{
+            span: 6,
+          }}
+          wrapperCol={{
+            span: 18,
+          }}
+          form={form}
+          name="inventory-form"
+          style={{
+            maxWidth: 600,
+          }}
+          autoComplete="off"
+          initialValues={formValues}
+          onFinish={onSubmit}
+        >
+          <Form.List name="items">
+            {(fields, { add, remove }) => (
+              <div
+                style={{
+                  display: "flex",
+                  rowGap: 16,
+                  flexDirection: "column",
+                }}
               >
-                <Form.Item label="Product name" name={[field.name, "product"]}>
-                  <Input />
-                </Form.Item>
-
-                <Form.Item label="Category #" name={[field.name, "category"]}>
-                  <InputNumber />
-                </Form.Item>
-
-                <Form.Item label="Product link" name={[field.name, "link"]}>
-                  <Input />
-                </Form.Item>
-
-                <Form.Item label="Image links">
-                  <Form.List name={[field.name, "images"]}>
-                    {(subFields, subOpt) => (
-                      <div
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          rowGap: 16,
+                {fields.map((field) => (
+                  <Card
+                    size="small"
+                    title={`Item ${field.name + 1}`}
+                    key={field.key}
+                    extra={
+                      <CloseOutlined
+                        onClick={() => {
+                          remove(field.name);
                         }}
-                      >
-                        {subFields.map((subField) => (
-                          <Space key={subField.key}>
-                            <Form.Item noStyle name={[subField.name, "link"]}>
-                              <Input />
-                            </Form.Item>
-                            <CloseOutlined
-                              onClick={() => {
-                                subOpt.remove(subField.name);
-                              }}
-                            />
-                          </Space>
-                        ))}
-                        <Button
-                          type="dashed"
-                          onClick={() => subOpt.add()}
-                          block
-                        >
-                          + Add image link
-                        </Button>
-                      </div>
-                    )}
-                  </Form.List>
-                </Form.Item>
+                      />
+                    }
+                  >
+                    <Form.Item
+                      label="Product name"
+                      name={[field.name, "product"]}
+                    >
+                      <Input />
+                    </Form.Item>
 
-                <Form.Item
-                  label="Retail price"
-                  name={[field.name, "price_retail"]}
-                >
-                  <InputNumber />
-                </Form.Item>
+                    <Form.Item
+                      label="Category #"
+                      name={[field.name, "category"]}
+                    >
+                      <InputNumber />
+                    </Form.Item>
 
-                <Form.Item label="Variants">
-                  <Form.List name={[field.name, "items"]}>
-                    {(subFields, subOpt) => (
-                      <div
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          rowGap: 16,
-                        }}
-                      >
-                        {subFields.map((subField) => (
-                          <Space key={subField.key}>
-                            <Form.Item
-                              label="Color"
-                              name={[subField.name, "color"]}
+                    <Form.Item label="Product link" name={[field.name, "link"]}>
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Image links">
+                      <Form.List name={[field.name, "images"]}>
+                        {(subFields, subOpt) => (
+                          <div
+                            style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              rowGap: 16,
+                            }}
+                          >
+                            {subFields.map((subField) => (
+                              <Space key={subField.key}>
+                                <Form.Item
+                                  noStyle
+                                  name={[subField.name, "link"]}
+                                >
+                                  <Input />
+                                </Form.Item>
+                                <CloseOutlined
+                                  onClick={() => {
+                                    subOpt.remove(subField.name);
+                                  }}
+                                />
+                              </Space>
+                            ))}
+                            <Button
+                              type="dashed"
+                              onClick={() => subOpt.add()}
+                              block
                             >
-                              <Input />
-                            </Form.Item>
-                            <Form.Item label="Sizes">
-                              <Form.List name={[subField.name, "sizes"]}>
-                                {(subSubFields, subSubOpt) => (
-                                  <div
-                                    style={{
-                                      display: "flex",
-                                      flexDirection: "column",
-                                      rowGap: 16,
-                                    }}
-                                  >
-                                    {subSubFields.map((subSubField) => (
-                                      <Space key={subSubField.key}>
-                                        <Form.Item
-                                          name={[subSubField.name, "size"]}
+                              + Add image link
+                            </Button>
+                          </div>
+                        )}
+                      </Form.List>
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Retail price"
+                      name={[field.name, "price_retail"]}
+                    >
+                      <InputNumber />
+                    </Form.Item>
+
+                    <Form.Item label="Variants">
+                      <Form.List name={[field.name, "items"]}>
+                        {(subFields, subOpt) => (
+                          <div
+                            style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              rowGap: 16,
+                            }}
+                          >
+                            {subFields.map((subField) => (
+                              <Space key={subField.key}>
+                                <Form.Item
+                                  label="Color"
+                                  name={[subField.name, "color"]}
+                                >
+                                  <Input />
+                                </Form.Item>
+                                <Form.Item label="Sizes">
+                                  <Form.List name={[subField.name, "sizes"]}>
+                                    {(subSubFields, subSubOpt) => (
+                                      <div
+                                        style={{
+                                          display: "flex",
+                                          flexDirection: "column",
+                                          rowGap: 16,
+                                        }}
+                                      >
+                                        {subSubFields.map((subSubField) => (
+                                          <Space key={subSubField.key}>
+                                            <Form.Item
+                                              noStyle
+                                              name={[subSubField.name, "size"]}
+                                            >
+                                              <Input />
+                                            </Form.Item>
+                                            <CloseOutlined
+                                              onClick={() => {
+                                                subSubOpt.remove(
+                                                  subSubField.name
+                                                );
+                                              }}
+                                            />
+                                          </Space>
+                                        ))}
+                                        <Button
+                                          type="dashed"
+                                          onClick={() => subSubOpt.add()}
+                                          block
                                         >
-                                          <Input />
-                                        </Form.Item>
-                                        <CloseOutlined
-                                          onClick={() => {
-                                            subSubOpt.remove(subSubField.name);
-                                          }}
-                                        />
-                                      </Space>
-                                    ))}
-                                    <Button
-                                      type="dashed"
-                                      onClick={() => subSubOpt.add()}
-                                      block
-                                    >
-                                      + Add size
-                                    </Button>
-                                  </div>
-                                )}
-                              </Form.List>
-                            </Form.Item>
-                            <CloseOutlined
-                              onClick={() => {
-                                subOpt.remove(subField.name);
-                              }}
-                            />
-                          </Space>
-                        ))}
-                        <Button
-                          type="dashed"
-                          onClick={() => subOpt.add()}
-                          block
-                        >
-                          + Add variant
-                        </Button>
-                      </div>
-                    )}
-                  </Form.List>
-                </Form.Item>
-              </Card>
-            ))}
+                                          + Add size
+                                        </Button>
+                                      </div>
+                                    )}
+                                  </Form.List>
+                                </Form.Item>
+                                <CloseOutlined
+                                  onClick={() => {
+                                    subOpt.remove(subField.name);
+                                  }}
+                                />
+                              </Space>
+                            ))}
+                            <Button
+                              type="dashed"
+                              onClick={() => subOpt.add()}
+                              block
+                            >
+                              + Add variant
+                            </Button>
+                          </div>
+                        )}
+                      </Form.List>
+                    </Form.Item>
+                  </Card>
+                ))}
 
-            <Button type="dashed" onClick={() => add()} block>
-              + Add Item
-            </Button>
-          </div>
-        )}
-      </Form.List>
+                <Button type="dashed" onClick={() => add()} block>
+                  + Add Item
+                </Button>
+              </div>
+            )}
+          </Form.List>
 
-      <Form.Item shouldUpdate>
-        {() => (
-          <Button
-            type="primary"
-            htmlType="submit"
-            disabled={!form.isFieldsTouched()}
-          >
-            Save
-          </Button>
-        )}
-      </Form.Item>
+          <Form.Item shouldUpdate>
+            {() => (
+              <>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={!form.isFieldsTouched()}
+                >
+                  Save
+                </Button>
+                <Button
+                  onClick={() => form.setFieldsValue(formValues)}
+                  disabled={!form.isFieldsTouched()}
+                >
+                  Restore last saved
+                </Button>
+              </>
+            )}
+          </Form.Item>
 
-      <Form.Item noStyle shouldUpdate>
-        {() => (
-          <Typography>
-            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
-          </Typography>
-        )}
-      </Form.Item>
-    </Form>
+          <Form.Item noStyle shouldUpdate>
+            {() => (
+              <Typography>
+                <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+              </Typography>
+            )}
+          </Form.Item>
+        </Form>
+      )}
+    </>
   );
 };
