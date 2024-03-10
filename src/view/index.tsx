43cdfccb1896e9f2c7044c8e4945e3fd7b8d4661
Matthew Huang <mhuang@figma.com>
2024-03-10 00:08:47 -0800

    fixes

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -3,10 +3,29 @@ import { createRoot } from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
+async function auth() {
+  const ind = window.location.hash.indexOf("credential=");
+  if (ind === -1) {
+    return;
+  }
+  const credential = window.location.hash.slice(ind).split("=")[1];
+  if (!credential) {
+    return;
+  }
+
+  window.location.href = window.location.href.split("?")[0];
+  await fetch(`/auth/login/${credential}`, {
+    method: "GET",
+  });
+  location.reload();
+}
+
+auth().then(() => {
+  const container = document.getElementById("root");
+  const root = createRoot(container!);
+  root.render(
+    <Router>
+      <App />
+    </Router>
+  );
+});
