18136a57dcfff8a2ac8746649e206b90d9a50a27
Diane <dianezhou96@gmail.com>
2024-03-13 21:57:55 -0700

    add fundraiser

--- src/constants.ts
+++ src/constants.ts
@@ -3,11 +3,13 @@
 // export const SHOP_NAME = "SFIT x Lululemon Black Friday Sale";
 export const SHOP_NAME = "SFIT x Lululemon Spring Sale";
 
-export const FUNDRAISER_LINK = "";
+export const FUNDRAISER_LINK =
+  "https://www.gofundme.com/f/sfit-nations-cup-2024";
 // "https://www.gofundme.com/f/sfit-nationals-sendoff-gift";
 // "https://www.gofundme.com/f/sfit-giving-2023";
 
-export const FUNDRAISER_IMAGE_LINK = "";
+export const FUNDRAISER_IMAGE_LINK =
+  "https://luludb.dianeyz.me/uploads/medium_sfit_nationscup2024_gofundme_59e8fa42f5.jpg";
 // "https://luludb.dianeyz.me/uploads/medium_Screen_Shot_2023_06_22_at_8_32_26_PM_2b19ebf2bd.png";
 // "https://luludb.dianeyz.me/uploads/medium_SFIT_Fundraiser_Giving2023_42c6b179e1.png";
 
