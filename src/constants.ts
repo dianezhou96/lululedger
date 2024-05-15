56fd12aff261f86d5a189e00c5453d2f10d4ea21
Diane <dianezhou96@gmail.com>
2024-05-15 16:36:30 -0700

    update data and variables for nationals 2024 sale

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,17 @@
 // export const SHOP_NAME = "SFIT x Lululemon Fundraiser";
 // export const SHOP_NAME = "SFIT x Lululemon Fall 2023 Flash Fundraiser";
 // export const SHOP_NAME = "SFIT x Lululemon Black Friday Sale";
-export const SHOP_NAME = "SFIT x Lululemon Spring Sale";
+// export const SHOP_NAME = "SFIT x Lululemon Spring Sale";
+export const SHOP_NAME = "SFIT x Lululemon Nationals 2024 Fundraiser";
 
 export const FUNDRAISER_LINK =
-  "https://www.gofundme.com/f/sfit-nations-cup-2024";
+  "https://www.gofundme.com/f/sfit-nationals-2024-sendoff-gift";
 // "https://www.gofundme.com/f/sfit-nationals-sendoff-gift";
 // "https://www.gofundme.com/f/sfit-giving-2023";
 
 export const FUNDRAISER_IMAGE_LINK =
-  "https://luludb.dianeyz.me/uploads/medium_sfit_nationscup2024_gofundme_59e8fa42f5.jpg";
+  "https://luludb.dianeyz.me/uploads/medium_sfit2024nationalsfundraiser_5c5ad0dc1b.jpg";
+// "https://luludb.dianeyz.me/uploads/medium_sfit_nationscup2024_gofundme_59e8fa42f5.jpg";
 // "https://luludb.dianeyz.me/uploads/medium_Screen_Shot_2023_06_22_at_8_32_26_PM_2b19ebf2bd.png";
 // "https://luludb.dianeyz.me/uploads/medium_SFIT_Fundraiser_Giving2023_42c6b179e1.png";
 
@@ -17,44 +19,48 @@ export const FUNDRAISER_CATEGORY_ID = 6;
 
 export const DISCOUNT = 0.4;
 
-export const DEADLINE = "Thursday, March 14 @ 9pm";
+export const DEADLINE = "Sunday, May 19 @ 6pm";
 
-// export const CLOSED = false;
-export const CLOSED = true;
+export const CLOSED = false;
+// export const CLOSED = true;
 // export const PREVIEW = true;
 export const PREVIEW = false;
-export const START_DATE = "Mon 3/11 ~1pm";
+export const START_DATE = "Wed 5/15 ~5pm";
 
 export const COLORS = [
   "Black",
   "Black/Black",
-  "Black/Gold",
-  "White",
-  "White/White",
   "True Navy",
+  "True Navy/True Navy",
+  "White",
   "Slate/White",
-  "Hot Heat/Hot Heat",
-  "Larkspur",
-  "Larkspur/Larkspur",
-  "Windmill/Windmill",
-  "Army Green/Army Green",
+  "Graphite Grey",
   "Heathered Core Ultra Light Grey",
-  "Trench",
-  "Orange Flash",
-  "Strawberry Milkshake",
-  "Dark Oxide",
-  "Obsidian",
-  "Silver Drop",
-  "Nomad",
-  "Shale Blue",
-  "Medium Forest",
-  "College Crimson",
-  "Harbor Blue",
-  "Butternut Brown",
+  "Heathered Graphite Grey",
+  "Heritage 365 Camo Deep Coal Multi",
+  "Peach Bellini",
+  "Sundance",
+  "Burnt Orange",
   "Maize Yellow",
-  "Brier Rose",
-  "College Purple",
+  "Oasis Blue",
+  "Strawberry Milkshake",
+  "College Purple/College Purple",
+  "College Cobalt/College Cobalt",
+  "College Crimson",
+  "College Crimson/College Crimson",
+  "Medium Forest",
+  "Orange Flash",
+  "Shade/Gold",
+  "Espresso",
   "Dark Olive",
+  "Magenta Purple",
+  "Butternut Brown",
+  "Belgian Blue",
+  "Silver Drop",
+  "Obsidian",
+  "Trench",
+  "Spiced Chai",
+  "Raw Linen",
 ];
 
 export const ITEM_SIZES = [
@@ -78,6 +84,10 @@ export const ITEM_SIZES = [
   "36",
   "38",
   "40",
+  "42",
+  "44",
+  "46",
+  "48",
   "XS/S",
   "M/L",
   "XL/XXL",
